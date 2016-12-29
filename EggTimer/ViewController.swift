e84f13a463d6435257977fe7267b90b1e4a11a3e
VEERASEKHAR ADDEPALLI <Veer@VEERASEKHARs-MacBook-Pro.local>
2016-12-29 01:40:53 -0500

    First commit

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -9,7 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var timer = Timer()
+    
+    var time = 210
+    
+    func decreaseTimer()
+    {
+        if time > 0{
+        time -= 1
+        
+        timerLabel.text = String(time)
+        }
+        else
+        {
+            timer.invalidate()
+        }
+    }
+    @IBOutlet var timerLabel: UILabel!
+    @IBAction func pause(_ sender: AnyObject) {
+        timer.invalidate()
+    }
+    @IBAction func play(_ sender: AnyObject) {
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.decreaseTimer), userInfo: nil, repeats: true)
+        
+    }
+   
+    @IBAction func minusTen(_ sender: AnyObject) {
+        if time > 10{
+        time -= 10
+        timerLabel.text = String(time)
+        }
+    }
+    @IBAction func plusTen(_ sender: AnyObject) {
+        time += 10
+        timerLabel.text = String(time)
+    }
+    @IBAction func reset(_ sender: Any) {
+        time = 210
+        timerLabel.text = String(time)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
